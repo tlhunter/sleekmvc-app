380e52aed3628880ad03842e39dc28ecc6a01cf1
Thomas Hunter <tlhunter@gmail.com>
2011-09-03 10:36:51 -0400

    Adding some response code (redirects et al)

--- system/Response.php
+++ system/Response.php
@@ -1,6 +1,18 @@
 <?php
 class Response {
+    public static function redirect($url, $permanent = FALSE) {
+        if ($permanent) {
+            header("HTTP/1.1 301 Moved Permanently");
+        }
+        header("Location: $url");
+        exit(); // is this a good idea?
+    }
 
+    public static function header($name, $value) {
 
+    }
 
+    public static function status($code) {
+
+    }
 }
