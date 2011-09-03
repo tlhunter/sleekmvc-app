89eae5c48114d5b54c9e954f57dd7554a8e8dfb5
Thomas Hunter <tlhunter@gmail.com>
2011-09-02 21:40:16 -0400

    Database will be a singleton

--- system/model/Database.php
+++ system/model/Database.php
@@ -1,5 +1,18 @@
 <?php
 abstract class Model_Database extends Model_Base {
     protected $connection = NULL;
+    private $instance = NULL;
+
+    private function __construct() {
+        // set connection using Config class
+    }
+
+    public static function getInstance() {
+        if (self::$instance) {
+            return self::$instance;
+        }
+        self::$instance = new Model_Database();
+        return self::$instance;
+    }
 
 }
