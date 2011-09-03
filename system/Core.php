e012c70b675b8ba5cca0f591d9967b01e6e3d425
Thomas Hunter <tlhunter@gmail.com>
2011-09-03 13:21:59 -0400

    Making the Core class easier to understand

--- system/Core.php
+++ system/Core.php
@@ -1,28 +1,27 @@
 <?php
 class Core {
-    protected $controller = NULL;
-    protected $action = NULL;
-    protected $arguments = NULL;
-    protected $cClassName = NULL;
+    protected $controllerName   = NULL;
+    protected $controller       = NULL;
+    protected $actionName       = NULL;
+    protected $arguments        = NULL;
 
     function __construct() {
-        $this->controller     = isset($_GET['controller']) ? $_GET['controller'] : Config::get('default_controller');
-        $this->action         = 'action_' . (isset($_GET['action']) ? $_GET['action'] : Config::get('default_action'));
-        $this->arguments      = isset($_GET['arg']) ? $_GET['arg'] : array();
-        $this->cClassName     = "Controller_{$this->controller}";
+        $this->controllerName   = 'Controller_' . (isset($_GET['controller']) ? $_GET['controller'] : Config::get('default_controller'));
+        $this->actionName       = 'action_' . (isset($_GET['action']) ? $_GET['action'] : Config::get('default_action'));
+        $this->arguments        = isset($_GET['arg']) ? $_GET['arg'] : array();
 
         try {
-            $this->controller = new $this->cClassName;
+            $this->controller = new $this->controllerName;
         } catch (ExceptionClassNotFound $e) {
-            self::throw404($this->controller, $this->action, $this->arguments);
+            self::throw404($controllerName, $this->actionName, $this->arguments);
         }
 
-        if (method_exists($this->controller, $this->action)) {
+        if (method_exists($this->controller, $this->actionName)) {
             $this->controller->preAction();
-            self::loadController($this->controller, $this->action, $this->arguments);
+            self::loadController($this->controller, $this->actionName, $this->arguments);
             $this->controller->postAction();
         } else {
-            self::throw404($this->controller, $this->action, $this->arguments);
+            self::throw404($controllerName, $this->actionName, $this->arguments);
         }
     }
 
