6f347a26532d0fb8ac4c3911cc10a383d97e49f6
Thomas Hunter <tlhunter@gmail.com>
2011-09-05 16:53:08 -0400

    Making some updates to email class

--- system/Email.php
+++ system/Email.php
@@ -1,27 +1,37 @@
 <?php
 class Email {
-    protected $recipient;
-    protected $subject;
-    protected $body;
-    protected $sender;
+    const HTML              = 'html';
+    const TEXT              = 'text';
 
-    public function __construct($recipient, $subject, $body, $sender) {
+    protected $recipient    = NULL;
+    protected $subject      = NULL;
+    protected $body         = NULL;
+    protected $sender       = NULL;
+    protected $lastError    = '';
+
+    public function __construct($recipient = NULL, $subject = NULL, $body = NULL, $sender = NULL) {
         if ($recipient) {
-            $this->recipient = $recipient;
+            $this->setRecipient($recipient);
         }
         if ($subject) {
-            $this->subject = $subject;
+            $this->setSubject($subject);
         }
         if ($body) {
-            $this->body = $body;
+            $this->setBody($body);
         }
         if ($sender) {
-            $this->sender = $sender;
+            $this->setSender($sender);
         }
     }
 
     public function setRecipient($recipient) {
-        $this->recipient = $recipient;
+        $recipient = trim($recipient);
+        if (!filter_var($recipient, FILTER_VALIDATE_EMAIL)) {
+            $this->recipient = NULL;
+            $this->lastError = "Invalid Email set as Recipient";
+        } else {
+            $this->recipient = $recipient;
+        }
         return $this;
     }
 
@@ -30,18 +40,42 @@ class Email {
         return $this;
     }
 
-    public function setBody($body) {
+    public function setBody($body, $type = NULL) {
+        // TODO: Handle TEXT/HTML emails
         $this->body = $body;
         return $this;
     }
 
     public function setSender($sender) {
-        $this->sender = $sender;
+        $recipient = trim($recipient);
+        if (!filter_var($sender, FILTER_VALIDATE_EMAIL)) {
+            $this->sender = NULL;
+            $this->lastError = "Invalid Email set as Sender";
+        } else {
+            $this->sender = $sender;
+        }
         return $this;
     }
 
-    public function send() {
-        return mail($this->recipient, $this->subject, $this->body, "From: {$this->sender}");
+    public function getLastError() {
+        return $this->lastError;
+    }
+
+    public function send($type = NULL) {
+        // TODO: Handle TEXT/HTML emails (if only one type is specified, use that if $type is NULL)
+        if ($this->recipient && $this->subject && $this->body && $this->sender) {
+            return mail($this->recipient, $this->subject, $this->body, "From: {$this->sender}");
+        }
+    }
+
+    public function attachFile($filename) {
+        // TODO: Handle file attachments
+        return $this;
+    }
+
+    public function attachFileFromString($filename, $content) {
+        // TODO: Handle fiel attachments
+        return $this;
     }
 
 }
