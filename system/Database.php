c16515af8376f4de3098155267b8bcbf82a2daf5
Thomas Hunter <tlhunter@gmail.com>
2011-09-05 13:29:55 -0400

    Making Database singleton, Model_Database loads it as a property

--- system/Database.php
+++ system/Database.php
@@ -1,4 +1,21 @@
 <?php
 class Database {
+    private $_instance = NULL;
+
+    private function __construct() {
+
+    }
+
+    private function __clone() {
+
+    }
+
+    public static function getInstance() {
+        if (!self::$_instance) {
+            self::$_instance = new Database();
+        }
+
+        return self::$_instance;
+    }
 
 }
--- system/model/Database.php
+++ system/model/Database.php
@@ -3,7 +3,7 @@ abstract class Model_Database extends Model_Base {
     protected $db = NULL;
 
     function __construct() {
-        $this->db = new Database();
+        $this->db = Database::getInstance();
     }
 
 }
