75b5801c16dc896413a63f5b20ba04782e3c6032
Thomas Hunter <tlhunter@gmail.com>
2011-09-05 21:33:49 -0400

    function visibility and making results countable

--- system/DatabaseResult.php
+++ system/DatabaseResult.php
@@ -1,5 +1,5 @@
 <?php
-class DatabaseResult /*implements Iterator */{
+class DatabaseResult implements Countable /*implements Iterator */ {
     protected $result = NULL;
     // This class will be used for iterating over database results
     public function __construct($databaseResult) {
@@ -7,16 +7,20 @@ class DatabaseResult /*implements Iterator */{
 
     }
 
-    function row() { // Associative Array (personal favorite)
+    public function row() { // Associative Array (personal favorite)
         return mysql_fetch_assoc($this->result);
     }
 
-    function enum() { // Enumerated Array (who would want this?!)
+    public function enum() { // Enumerated Array (who would want this?!)
         return mysql_fetch_row($this->result);
     }
 
-    function object() { // Object (std class)
+    public function object() { // Object (std class)
         return mysql_fetch_object($this->result);
+    }
+
+    public function count() {
+        return mysql_num_rows($this->result);
     }
 
 }
