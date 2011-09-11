19bee9d77affed53f249200f2a19c0bb289e16a7
Thomas Hunter <tlhunter@gmail.com>
2011-09-11 12:49:43 -0400

    Still trying to handle 500 errors... Probably not gonna happen.

--- system/Error.php
+++ system/Error.php
@@ -4,18 +4,19 @@ class Error {
         $errorClassName = 'Controller_' . Config::get('error_controller');
         $errorClass = new $errorClassName;
         $errorClass->action_500($errno, $errstr, $errfile, $errline, $errcontext);
+        exit();
     }
 
     public static function shutdown() {
         $error = error_get_last();
         if ($error['type'] == 1) {
+            //include_once(APP_PATH . 'controller/Error.php');
             $errorClassName = 'Controller_' . Config::get('error_controller');
             $errorClass = new $errorClassName;
-            $errorClass->action_500($error);
+            $errorClass->action_fatal($error);
         }
     }
 }
 
-
-set_error_handler(array('Error', 'handler'));
+set_error_handler(array('Error', 'handler')); // Catches generic errors (or so I'm told)
 //register_shutdown_function(array('Error', 'shutdown')); // Catches Fatal errors
