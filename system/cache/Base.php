c2be30e997e35e88550ab10238e67d98123b5930
Thomas Hunter <tlhunter@gmail.com>
2011-09-11 12:32:28 -0400

    Documentation

--- system/cache/Base.php
+++ system/cache/Base.php
@@ -1,10 +1,38 @@
 <?php
 interface Cache_Base {
+    /**
+     * Sets data $cache->set($key, $value)
+     * @param $key string
+     * @param $value mixed
+     */
     function set($key, $value);
-    function get($key);
-    function __set($key, $value);
-    function __get($key);
-    function replace($key, $value);
-    function delete($key);
 
+    /**
+     * Gets data $cache->get($key)
+     * @param $key string
+     */
+    function get($key);
+
+    /**
+     * Sets data $cache->key = $value
+     */
+    function __set($key, $value);
+
+    /**
+     * Gets data $value = $cache->key
+     */
+    function __get($key);
+
+    /**
+     * Replaces data with a new value $cache->replace($key, $value)
+     * @param $key string
+     * @param $value mixed
+     */
+    function replace($key, $value);
+
+    /**
+     * Deletes a key from the cache $cache->delete($key)
+     * @param $key string
+     */
+    function delete($key);
 }
