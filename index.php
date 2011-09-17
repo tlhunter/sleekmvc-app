78bc6888c084ed740ddb8c288331d92d557ceffd
Thomas Hunter <tlhunter@gmail.com>
2011-09-17 16:28:22 -0400

    Putting in a check for PHP 5.3

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+if (version_compare(PHP_VERSION, '5.3.0') < 0) {
+    die("SleekMVC Requires PHP 5.3+");
+}
+
 define('SYSTEM_DIR',    'system/');
 define('APP_DIR',       'app/');
 define('VENDOR_DIR',    'vendor/');
