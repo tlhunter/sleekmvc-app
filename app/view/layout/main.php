b5cb5d5d1dc116662d220a98e2b1421a69350276
Thomas Hunter <tlhunter@gmail.com>
2011-09-19 20:05:42 -0400

    Making it obvious when pages use the layout view

--- app/view/layout/main.php
+++ app/view/layout/main.php
@@ -1,6 +1,14 @@
 <html>
 <head>
 <title><?=$title?></title>
+<style>
+body {
+    background-color: #000;
+    color: #fff;
+    font-family: "Lucida Sans", arial;
+    font-size: 12px;
+}
+</style>
 </head>
 <body>
 <?=$content?>
